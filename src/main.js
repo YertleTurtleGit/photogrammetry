1bcf0ee2bb5dba9deeb2ffa80326a443cfc7b8f1
YertleTurtleGit <34098804+YertleTurtleGit@users.noreply.github.com>
2021-09-21 16:38:26 +0000

    Deploying to web from @ YertleTurtleGit/photogrammetry@fe2c92696995187114353ad6d5dbc0f0d1e914bc 🚀

---
+++ src/main.js
@@ -0,0 +1,1 @@
+async function loadImage(e){const a=new Image;return new Promise((t=>{a.addEventListener("load",(()=>{t(a)})),a.addEventListener("error",(()=>{t(void 0)})),a.src=e}))}async function main(){const e=await loadImage("./test-dataset/a.jpg"),a=await loadImage("./test-dataset/b.jpg"),t=await StereoDepthHelper.getDepthMapping(e,a);document.body.appendChild(t)}main();
